a9cc3b5171470b2cb96c688b34e39ad39b069c31
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-05-31 17:15:57 +0200

    refactor( components ): add micro-stack and micro-stage components to help making transition between stages easily

---
+++ src/components/micro-stack.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+    
+.micro-stack: slot
+
+</template>
+<script>
+import { computed } from 'vue';
+
+export default {
+    
+    data: vm => ({
+
+        head: 0,
+
+        stages: []
+
+    }),
+
+    methods: {
+
+        async nextStage () {
+
+            await this.stages[this.head].$attrs.onLeave();
+
+            this.head += 1;
+
+        }
+
+    },
+
+    provide () {
+
+        return { head: computed(() => this.head), stages: this.stages }
+
+    }
+
+}
+</script>
---
+++ src/components/micro-stage.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+    
+.micro-stack__stage( v-show="head === index" ): slot
+
+</template>
+<script>
+export default {
+
+    data: vm => ({
+
+        index: vm.stages.length
+
+    }),
+
+    watch: {
+
+        head: {
+
+            async handler (head) {
+
+                if ( head === this.index && this.$attrs.onEnter ) await this.$attrs.onEnter()
+
+            },
+
+            immediate: true
+
+        }
+
+    },
+
+    created () {
+
+        this.stages.push(this);
+
+    },
+
+    inject: ['head', 'stages']
+
+}
+</script>
