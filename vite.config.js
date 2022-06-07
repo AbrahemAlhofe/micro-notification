f0dc5cd420ebc7c5a1817f68b0d9f06b79bec2a4
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-06-07 22:27:16 +0200

    build: move /src/assets to /dist

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import SvgLoader from 'vite-svg-loader'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [
     vue(),
-    SvgLoader()
+    SvgLoader(),
+    viteStaticCopy({
+      targets: [
+          {
+            src: 'src/assets/avatars',
+            dest: 'assets'
+          },
+          {
+            src: 'src/assets/icons',
+            dest: 'assets'
+          }
+      ]
+    })
   ]
 })
